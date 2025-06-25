81542db8b78b1e93877ae75d4bedb59508278144
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-06-25 12:59:34 +0100

    add workflow for vendor unit tests

---
+++ src/subtract.ts
@@ -0,0 +1,9 @@
+/**
+ * Subtracts two numbers and returns the result
+ * @param a First number
+ * @param b Second number to subtract from first number
+ * @returns The difference between a and b
+ */
+export function subtract(a: number, b: number): number {
+  return a - b
+}
