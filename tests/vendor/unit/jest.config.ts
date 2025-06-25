a111e617bc331b4f64750a8abd4a04c9f2d443ad
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-06-25 12:51:04 +0100

    update test config to search for `vendor` files

--- tests/vendor/unit/jest.config.ts
+++ tests/vendor/unit/jest.config.ts
@@ -1,19 +1,26 @@
 import { JestConfigWithTsJest } from 'ts-jest'
 
 const containerBasePath = 'examples/express-container'
+const vendorBasePath = '**/vendor'
 
 const config: JestConfigWithTsJest = {
   verbose: true,
   rootDir: '../../../',
-  testMatch: [`<rootDir>/${containerBasePath}/**/*.test.ts`],
+  testMatch: [
+    `<rootDir>/${containerBasePath}/**/*.test.ts`,
+    `<rootDir>/${vendorBasePath}/**/*.test.ts`
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: [
     `${containerBasePath}/**/*.ts`,
+    `${vendorBasePath}/**/*.ts`,
     // Exclude test files from coverage
-    `!${containerBasePath}/**/*.test.ts`
+    `!${containerBasePath}/**/*.test.ts`,
+    `!${vendorBasePath}/**/*.test.ts`,
+    '!**/*/jest.config.ts'
   ],
-  coverageDirectory: `coverage/${containerBasePath}`,
+  coverageDirectory: `coverage/vendor`,
   collectCoverage: true,
   coverageReporters: ['text']
 }
