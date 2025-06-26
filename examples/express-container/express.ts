f3c2ecc7ec817f5a7142bd97fea2d59b86583ef0
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-06-26 10:08:15 +0100

    move running of server into separate file

--- examples/express-container/express.ts
+++ examples/express-container/express.ts
@@ -1,16 +1,12 @@
 import express, { Request, Response } from 'express'
 
-const port = process.env['PORT'] ?? 3000
-
 const app = express()
 const v1Router = express()
 
 v1Router.get('/:id', (req: Request, res: Response) => {
-  res.json({ id: req.params['id'] })
+  res.json({ id: Number(req.params['id']) })
 })
 
 app.use('/v1', v1Router)
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port.toString()}`)
-})
+export { app }
