5bcbad41be3969e40f523a0284ed92d920e0a27c
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-06-26 10:12:06 +0100

    add an example express test

--- examples/express-container/express.test.ts
+++ examples/express-container/express.test.ts
@@ -1,5 +1,9 @@
-describe('math', () => {
-  it('test math', () => {
-    expect(1).toBe(1)
+import { app } from './express'
+import request from 'supertest'
+
+describe('testing ExpressJS server', () => {
+  it('ensure id is returned correctly', async () => {
+    const response = await request(app).get('/v1/5')
+    expect(response.body).toStrictEqual({ id: 5 })
   })
 })
