0b0c709356242a8e8a4ec2bfb2d4450f8f82f188
Josh Speed <joshua.speed@digital.cabinet-office.gov.uk>
2025-06-27 17:19:32 +0100

    Create auth endpoint

--- examples/express-container/server.ts
+++ examples/express-container/server.ts
@@ -1,4 +1,8 @@
 import { app } from './express'
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 const port = process.env['PORT'] ?? 3000
 
