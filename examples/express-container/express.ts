352d57809dcdd12333632b799cae99383baf4106
Josh Speed <joshua.speed@digital.cabinet-office.gov.uk>
2025-06-27 17:19:32 +0100

    Create auth endpoint

--- examples/express-container/express.ts
+++ examples/express-container/express.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express'
+import { auth } from '../../src/auth/auth'
 
 const app = express()
 const v1Router = express()
 
-v1Router.get('/:id', (req: Request, res: Response) => {
-  res.json({ id: Number(req.params['id']) })
+v1Router.get('/auth', (req: Request, res: Response) => {
+  auth(req, res)
 })
 
 app.use('/v1', v1Router)
