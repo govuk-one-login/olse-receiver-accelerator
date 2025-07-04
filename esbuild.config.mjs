304c831590d1b30dfb8d1b9f56cab950ff2cc8d7
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-07-04 10:41:47 +0100

    add feature branch deployment with example lambdas

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,5 +1,8 @@
 // esbuild.config.js
 import esbuild from 'esbuild'
+import { readFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { yamlParse } from 'yaml-cfn'
 
 const baseEsBuildConfig = {
   bundle: true,
@@ -19,14 +22,54 @@ async function main() {
   if (process.env['CONTAINER'] === 'true') {
     console.log('Running esbuild for container')
     await buildForContainer()
+  } else if (process.env['AWS_LAMBDA_REFERENCE'] === 'true') {
+    console.log('Running esbuild for container')
+    await buildFor_AWS_LAMBDA_REFERENCE()
+  } else {
+    throw new Error('Invalid build target')
   }
 }
 
-async function buildForContainer() {
+async function buildFor_AWS_LAMBDA_REFERENCE() {
+  const baseLambdaPath = 'examples/aws-lambda'
+  const lambdasPath = `${baseLambdaPath}/src/lambda`
+  const { Resources } = yamlParse(
+    readFileSync(join(dirname('.'), `${baseLambdaPath}/template.yaml`), 'utf-8')
+  )
+  // const awsResources = Object.values(Resources) as IAwsResource[]
+  const awsResources = Object.values(Resources)
+
+  //   const lambdas = awsResources.filter(
+  //   (resource) => resource.Type === 'AWS::Serverless::Function'
+  // ) as ILambdaFunction[]
+  const lambdas = awsResources.filter(
+    (resource) => resource.Type === 'AWS::Serverless::Function'
+  )
+  const entries = lambdas.reduce((entryPoints, lambda) => {
+    const lambdaName = lambda.Properties.CodeUri.split('/').pop()
+    if (!(lambdaName in entryPoints)) {
+      entryPoints.push(`./${lambdasPath}/${lambdaName}/handler.ts`)
+    }
+
+    return entryPoints
+  }, [])
+
+  console.log('entries')
+  console.log(entries)
   const finalConfig = {
     ...baseEsBuildConfig,
-    entryPoints: ['examples/express-container/server.ts'],
-    outfile: 'dist/examples/express-container/server.js'
+    entryPoints: entries,
+    outdir: `dist/${lambdasPath}`
+  }
+  await esbuild.build(finalConfig)
+}
+
+async function buildForContainer() {
+  const basePath = 'examples/express-container'
+  const finalConfig = {
+    ...baseEsBuildConfig,
+    entryPoints: [`${basePath}/server.ts`],
+    outfile: `dist/${basePath}/server.js`
   }
   try {
     const context = await esbuild.context(finalConfig)
