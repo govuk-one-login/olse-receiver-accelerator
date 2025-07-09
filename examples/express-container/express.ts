a48afec43def2b39d816843b2cf8fce98514e984
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-07-09 11:14:00 +0100

    fix dev mode due to ESM migration

--- examples/express-container/express.ts
+++ examples/express-container/express.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express'
-import { auth } from '../../src/auth/auth'
-
-
+// import { auth } from '../../src/auth/auth'
 
 const app = express()
 const v1Router = express()
 
 v1Router.get('/auth', (req: Request, res: Response) => {
-  auth(req, res)
+  const v = 'fax'
+  res.json({ hello: v, time: new Date().toISOString() })
+  console.log(v)
 })
 
 app.use('/v1', v1Router)
