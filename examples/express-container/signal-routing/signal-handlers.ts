7260ac883094f8a75672055ddecee0486c35a5b1
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-07-14 14:12:01 +0100

    chore: remove unused code

--- examples/express-container/signal-routing/signal-handlers.ts
+++ examples/express-container/signal-routing/signal-handlers.ts
@@ -12,18 +12,3 @@ export function handleAccountDisabled(_setPayload: SetPayload): SignalResult {
     message: 'Account disabled.'
   }
 }
-// export function handleAccountPurged(
-//   _setPayload: SetPayload,
-//   _req: SetRequest,
-//   res: Response
-// ): void {
-//   res.status(200).json({ success: true, message: 'Account purged.' })
-// }
-
-// export function handleAccountDisabled(
-//   _setPayload: SetPayload,
-//   _req: SetRequest,
-//   res: Response
-// ): void {
-//   res.status(200).json({ success: true, message: 'Account disabled.' })
-// }
