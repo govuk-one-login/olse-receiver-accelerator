59440019e089e30abaf5d2401055defcf38d68b4
Zayd Patel <zayd.patel@digital.cabinet-office.gov.uk>
2025-07-14 15:32:47 +0100

    remove placeholder files

--- src/subtract.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Subtracts two numbers and returns the result
- * @param a First number
- * @param b Second number to subtract from first number
- * @returns The difference between a and b
- */
-export function subtract(a: number, b: number): number {
-  return a - b
-}
