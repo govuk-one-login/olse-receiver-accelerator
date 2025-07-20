62c6260d9df4ff7b546f673334afd2a1ba1dfa80
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-07-20 14:49:04 +0100

    revert: revert schedule changes from st

--- examples/express-container/config/config.ts
+++ examples/express-container/config/config.ts
@@ -2,9 +2,10 @@ import { VerificationTxConfig } from "../interfaces/interfaces";
 
 export const config: VerificationTxConfig = {
     JWT_SECRET: process.env["JWT_SECRET"] ?? 'default',
-    CRON_SCHEDULE: process.env["CRON_SCHEDULE"] ?? '*/15 * * * *',
+    VERIFICATION_INTERVAL: 60,
     ISSUER: process.env["ISSUER"] ?? 'https://gds.com',
     RELYING_PARTY_URL: process.env["RELYING_PARTY_URL"] ?? 'relying_party_url',
+    STREAM_ID: process.env["STREAM_ID"] ?? 'no stream id'
 }
 
 
