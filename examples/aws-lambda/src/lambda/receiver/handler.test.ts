ab5970bd4a0825d3dbd818d28705b3bca60ce636
Thomas Fuller <thomas.fuller@digital.cabinet-office.gov.uk>
2025-07-24 14:41:33 +0100

    SET-22 updated jest.config, added aws secrets manager for client_id and secret, adds intergration tests for reciever function

---
+++ examples/aws-lambda/src/lambda/receiver/handler.test.ts
@@ -0,0 +1,36 @@
+import { getTokenFromCognito } from './getTokenFromCognito'
+
+describe('handler V1', () => {
+  it('handler returns 200 when a valid access token is provided', async () => {
+    const token = await getTokenFromCognito()
+    console.log('Bearer ' + token)
+    const response = await fetch(
+      'https://ck4o4vjwgb.execute-api.eu-west-2.amazonaws.com/dev/api/v1/Events',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      }
+    )
+    console.log(response)
+    console.log(await response.json())
+    expect(response.status).toStrictEqual(200)
+  })
+  it('handler returns 401 when no valid access token is provided', async () => {
+    const token = await getTokenFromCognito()
+    console.log('Bearer ' + token)
+    const response = await fetch(
+      'https://ck4o4vjwgb.execute-api.eu-west-2.amazonaws.com/dev/api/v1/Events',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer thisisnot.avalid.accesstoken'
+        }
+      }
+    )
+    console.log(response)
+    console.log(await response.json())
+    expect(response.status).toStrictEqual(401)
+  })
+})
