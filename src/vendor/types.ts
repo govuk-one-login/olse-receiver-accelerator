5617b2c6c24d227abe0daa688138a41ab09f322b
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-08-08 09:09:01 +0100

    fix: type errors

--- src/vendor/types.ts
+++ src/vendor/types.ts
@@ -1,10 +1,8 @@
-import { SET } from '../../examples/mock-transmitter/types'
-
 export interface generateJWTPayload {
   issuer: string
   audience: string
   jti: string
-  payload: Record<string, unknown> | SET
+  payload: Record<string, unknown>
   alg: string
   useExpClaim: boolean
 }
