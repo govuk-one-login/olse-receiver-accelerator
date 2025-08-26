7e9a2dbecddba1655299b1c5a0f4ffb04007e7ce
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-08-26 14:07:42 +0100

    refine: clean up interfaces

--- examples/aws-lambda/src/mock-transmitter/mockApiTxInterfaces.ts
+++ examples/aws-lambda/src/mock-transmitter/mockApiTxInterfaces.ts
@@ -1,24 +1,3 @@
-export interface AuthRequest {
-  tokenEndpointClientId: string
-  body: string | null
-}
-
-export type AuthContext = Record<string, string | number | boolean | undefined>
-
-export interface AuthResult {
-  isAuthorised: boolean
-  principalId?: string
-  context?: AuthContext
-  error?: string
-}
-
-export interface ClientCredentials {
-  grant_type: string
-  client_id: string
-  client_secret: string
-  scope?: string
-}
-
 export interface RequestBody {
   stream_id: string
   state?: string
@@ -29,41 +8,7 @@ export interface KmsPublicKeyData {
   publicKey: Uint8Array
 }
 
-export interface PolicyStatement {
-  Effect: 'Allow' | 'Deny'
-  Action: string
-  Resource: string
-}
-
-export interface AuthPolicy {
-  Version: string
-  Statement: PolicyStatement[]
-}
-
-export interface AuthResponse {
-  principalId: string
-  policyDocument: AuthPolicy
-  context?: AuthContext
-}
-
 export interface SETVerificationRequest {
   stream_id: string
   state: string | undefined
-}
-
-export interface SET {
-  iss: string
-  aud: string
-  iat: number
-  jti: string
-  events: Record<
-    string,
-    {
-      state?: string
-    }
-  >
-  sub_id: {
-    format: 'opaque'
-    id: string
-  }
 }
