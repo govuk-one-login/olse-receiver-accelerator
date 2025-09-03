3024a1e040b1287c9117588a379fd35b5e924a12
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-09-03 13:43:36 +0100

    debug

--- tests/vendor/helpers/getTokenFromCognito.ts
+++ tests/vendor/helpers/getTokenFromCognito.ts
@@ -4,25 +4,29 @@ export const getTokenFromCognito = async (secretArn: string) => {
   const secrets = await getSecrets(secretArn)
   const { userPoolClientId, userPoolClientSecret, domain } = secrets
   console.log('Domain:', domain)
-  const accessTokenResponse = await fetch(
-    `https://${domain}.auth.eu-west-2.amazoncognito.com/token`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        client_id: userPoolClientId,
-        client_secret: userPoolClientSecret,
-        grant_type: 'client_credentials'
-      })
-    }
-  )
+  try {
+    const accessTokenResponse = await fetch(
+      `https://${domain}.auth.eu-west-2.amazoncognito.com/token`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          client_id: userPoolClientId,
+          client_secret: userPoolClientSecret,
+          grant_type: 'client_credentials'
+        })
+      }
+    )
 
-  const accessTokenJsonResponse = await accessTokenResponse.json()
-  // @ts-expect-error no-unknown
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const accessToken: string = accessTokenJsonResponse.access_token
-
-  return accessToken
+    const accessTokenJsonResponse = await accessTokenResponse.json()
+    // @ts-expect-error no-unknown
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const accessToken: string = accessTokenJsonResponse.access_token
+    return accessToken
+  } catch (error) {
+    console.error('Error fetching access token:', error)
+    throw error
+  }
 }
