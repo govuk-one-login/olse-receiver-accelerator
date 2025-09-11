1508d66d2dfdd363bf6b8c5a30eb8730daa26652
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-09-11 14:52:27 +0100

    wip: add ssm client

---
+++ common/ssm/ssm.ts
@@ -0,0 +1,26 @@
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm'
+import { getEnv } from '../../examples/aws-lambda/src/mock-transmitter/utils'
+import { ConfigurationKeys } from '../config/configurationKeys'
+
+const ssmClient = new SSMClient({
+  region: getEnv(ConfigurationKeys.AWS_REGION)
+})
+
+export const getParameter = async (parameterName: string): Promise<string> => {
+  try {
+    const command = new GetParameterCommand({
+      Name: parameterName
+    })
+
+    const response = await ssmClient.send(command)
+
+    if (!response.Parameter?.Value) {
+      throw new Error(`Parameter ${parameterName} not found or has no value`)
+    }
+
+    return response.Parameter.Value
+  } catch (error) {
+    console.error('Failed to get parameter ${parameterName}:', { error: error })
+    throw error
+  }
+}
