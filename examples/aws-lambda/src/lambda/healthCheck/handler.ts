00df2834852d811e51955b9e9c8a765c56dc044e
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-09-15 09:41:38 +0100

    wip: get endpoint url dynamically

--- examples/aws-lambda/src/lambda/healthCheck/handler.ts
+++ examples/aws-lambda/src/lambda/healthCheck/handler.ts
@@ -6,15 +6,13 @@ import {
 import { sendVerificationSignal } from '../../../../express-container/verification/sendVerification'
 import { config } from '../../../../express-container/config/globalConfig'
 import { ConfigurationKeys } from '../../../../express-container/config/ConfigurationKeys'
+import { getParameter } from '../../../../../common/ssm/ssm'
+import { getEnv } from '../../mock-transmitter/utils'
 
 const pause = (timeInMs: number) => {
   return new Promise((resolve) => setTimeout(resolve, timeInMs))
 }
 
-const VERIFICATION_ENDPOINT_URL = config.getOrDefault(
-  ConfigurationKeys.VERIFICATION_ENDPOINT_URL,
-  'https://rp.co.uk/verify'
-)
 const STREAM_ID = config.getOrDefault(
   ConfigurationKeys.STREAM_ID,
   'default-stream-id'
@@ -26,13 +24,17 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   try {
     console.log(event)
-    console.log('Verification endpoint url', VERIFICATION_ENDPOINT_URL)
+    const stackName = getEnv(ConfigurationKeys.AWS_STACK_NAME)
+    const verificationEndpointUrl = await getParameter(
+      `/${stackName}/receiver-endpoint`
+    )
+    console.log('Verification endpoint url', verificationEndpointUrl)
 
     // add a pause to prevent eslint from raising issues around the lack of an await function
     await pause(10)
 
     const response = await sendVerificationSignal(
-      VERIFICATION_ENDPOINT_URL,
+      verificationEndpointUrl,
       STREAM_ID
     )
     // Return successful response
