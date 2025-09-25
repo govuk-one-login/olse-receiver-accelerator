9d1ec3078efb2b6d38d8d6aa7382ad64c2ef0136
Thomas Brown <thomas.brown@digital.cabinet-office.gov.uk>
2025-09-25 14:23:18 +0100

    pr: address comments

--- examples/express-container/server.ts
+++ examples/express-container/server.ts
@@ -1,13 +1,11 @@
 import { app } from './express'
-import { configReady } from '../../common/config/config'
 import { baseLogger as logger } from '../../common/logging/logger'
 
-async function main(): Promise<void> {
-  await configReady()
+function main(): void {
   const port = '3000'
   app.listen(port, () => {
     logger.info(`Server running at http://localhost:${port}`)
   })
 }
 
-void main()
+main()
